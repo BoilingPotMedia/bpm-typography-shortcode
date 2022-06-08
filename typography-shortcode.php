94131316c856665e62922768469e8ae4d6e2c9d7
James Ryven Valeii <Jryven@gmail.com>
2022-06-08 13:39:32 -0400

    Update typography-shortcode.php
    removing undefined function and escaping correctly

--- typography-shortcode.php
+++ typography-shortcode.php
@@ -1,83 +1,71 @@
 <?php
-/*
-Plugin Name: Typography Shortcode
-Description: Wrap text in span containers with CSS classes.
-Author: Boiling Pot Media
-Version: 2.0
-License: 
-*/
+/**
+ * Plugin Name: Typography Shortcode
+ * Plugin URI: http://boilingpotmedia.com
+ * Description: Wrap text in span containers with CSS classes.
+ * Version: 2.0.1
+ * Author: James Valeii
+ * Author URI: http://jamesvaleii.com/
+ * Text Domain: typography
+ *
+ * @package typography
+ */
 
+
+/**
+ * TODO: Ensure naming conventions follow PHCPS? Stadnards
+ */
 class Inline__Typography {
 
+	public function __construct() {
+		$this->Inline__Typography__Callback();
+	}
 
-    public function __construct() {
-    	$this->Inline__Typography__Callback();
-    }   
-		
+	public function Inline__Typography__Callback() {
+		add_shortcode( 'f', array( $this, 'bpm__content__f_shortcode_callback' ) );
+	}
 
-    public function Inline__Typography__Callback(){
-        add_shortcode( 'f' , array( $this, 'bpm__content__f_shortcode_callback'));    
-    }
-    
-
- 	/* bpm__content__f_shortcode_callback
+	/** bpm__content__f_shortcode_callback
 	 *
 	 * Usage
 	 * [f &params ]
 	 *
 	 * Params
 	 *
-	 * @ e			- (optional) element - pass any valid HTML elm. Default: span
-	 * @ c			- (optional) class - pass any CSS classes as string. EG: c="tag green"
-	 * @ i			- (optional) inline-css - pass any CSS styles as string. EG: i="font-size:20px;color:red;"
-	 */	
-	public function bpm__content__f_shortcode_callback( $atts, $content = NULL ) {
+	 * @ e            - (optional) element - pass any valid HTML elm. Default: span
+	 * @ c            - (optional) class - pass any CSS classes as string. EG: c="tag green"
+	 * @ i            - (optional) inline-css - pass any CSS styles as string. EG: i="font-size:20px;color:red;"
+	 */
+	public function bpm__content__f_shortcode_callback( $atts, $content = null ) {
 
 		$a = shortcode_atts( array(
 			'e' => '',
 			'c' => '',
 			'i' => '',
-		), $atts );		
+		), $atts );
 
-		$id = bpm_make_id(5);
-		
-		$element = $atts['e'] ?? 'span';		
-		$classes = $atts['c'] ?? '';
-		$inline = $atts['i'] ?? '';		
-		
+		$element = $a['e'] ?? 'span';
+		$classes = $a['c'] ?? '';
+		$inlines = $a['i'] ?? '';
 		ob_start();
 		?>
-		
-		<?php echo '<'.$element.' class="'. $classes .'" style="'. $inline .'" >'; ?>
-			<?php echo do_shortcode($content); ?>
-		<?php echo '</'.$element.'>'; ?>
-		
+
+		<?php echo '<' . esc_attr( $element ) . ' class="' . esc_attr( $classes ) . '" style="' . esc_attr( $inlines ) . '" >'; ?>
+		<?php echo do_shortcode( force_balance_tags( wp_kses_post( $content ) ) ); ?>
+		<?php echo '</' . esc_attr( $element ) . '>'; ?>
+
 		<?php return ob_get_clean(); ?>
-	<?php
+		<?php
 	}
 }
 
+/**
+ * Call the Inline__Typography() Class to activate shortcode
+ */
 
-
-/* Call the Inline__Typography() Class to activate shortcode
- *
- *  
- */	
-
-add_action('wp_head','activate_f_shortcode');
-function activate_f_shortcode(){
+add_action( 'wp_head', 'activate_f_shortcode' );
+function activate_f_shortcode() {
 
 	new Inline__Typography();
 
 }
-
-
-
-
-
-
-
-
-
-
-/* the cold, final swig of coffee */ ?>
